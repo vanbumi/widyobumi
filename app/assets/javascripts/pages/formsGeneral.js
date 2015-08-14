d6cb059bc588f228dfa419e0b71379da9483f0e2
Dyo Medio <jsp.dio@gmail.com>
2015-08-14 11:33:18 +0700

    add theme pro ui

---
+++ app/assets/javascripts/pages/formsGeneral.js
@@ -0,0 +1,13 @@
+/*
+ *  Document   : formsGeneral.js
+ *  Author     : pixelcave
+ */
+var FormsGeneral = function () {
+    return {
+        init: function () {
+            $(".toggle-bordered").click(function () {
+                $(this).parents(".block").find("form").toggleClass("form-bordered")
+            })
+        }
+    }
+}();
